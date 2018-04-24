249c3fd215bd46660e4c8b2ff9f54cdba71dfa36
jiwen1 <jiwen1@jd.com>
2018-04-24 13:14:26 +0800

    消费服务111

--- src/main/java/com/demo/test/Test.java
+++ src/main/java/com/demo/test/Test.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.demo.service.ConsumerService;
 
 public class Test {
-	
+	this is brance
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext applicationContext=new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 //		ProviderService service=(ProviderService) applicationContext.getBean("providerService");
